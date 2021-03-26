4b633dfc2fb446943ef6132143bf9cf29c58e030
Andrew <andsof123455@gmail.com>
2021-03-26 14:37:04 +0200

    Add angular structure

--- PaymentRegister/ClientApp/src/app/app.module.ts
+++ PaymentRegister/ClientApp/src/app/app.module.ts
@@ -5,10 +5,15 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { PaymentDetailsComponent } from './payment-details/payment-details.component';
+import { PaymentDetailFormComponent } from './payment-details/payment-detail-form/payment-detail-form.component';
+import { PaymentDetailService } from './shared/payment-detail.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaymentDetailsComponent,
+    PaymentDetailFormComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -18,7 +23,7 @@ import { AppComponent } from './app.component';
       //{ path: '', pathMatch: 'full' }
     ])
   ],
-  providers: [],
+  providers: [PaymentDetailService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
